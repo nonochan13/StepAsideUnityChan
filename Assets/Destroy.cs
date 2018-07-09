12c9682b7ca5b205b85a064544148ad44e2aedd3
nonochan13 <ueno.jit@gmail.com>
2018-07-09 21:19:04 +0900

    StepAsideUnityChanプロジェクト　初めてのコミット 基本課題のみ

---
+++ Assets/Destroy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    //Unity
+    private void OnTriggerEnter(Collider other)
+    {
+        //障害物に衝突した場合（追加）
+        if (other.gameObject.tag == "CarTag" || other.gameObject.tag == "TrafficConeTag" || other.gameObject.tag == "CoinTag")
+        {
+            //接触したコインのオブジェクトを破棄（追加）
+            Destroy(other.gameObject);
+        }
+    }
+}
